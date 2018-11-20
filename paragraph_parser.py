dae10e63132c37ec99219b577615361bcadb0132
Arturo Fornés Arvayo <fornesarturo@gmail.com>
2018-11-19 19:12:15 -0600

    Parsing most to ruby
    Missing:
    * Relational expression.
    * Tabulation

--- paragraph_parser.py
+++ paragraph_parser.py
@@ -23,7 +23,8 @@ def class_declaration(tokens: List[Token]):
     check_token(tokens[2], IS)
     check_token(tokens[3], A)
     check_token(tokens[4], CLASS)
-    return tokens[5:]
+    text = "class " + tokens[1].value().capitalize() + "\n"
+    return tokens[5:], text
 
 def factor(tokens):
     if tokens[0].isK(ID) or tokens[0].isK(NUM):
@@ -74,41 +75,62 @@ def simple_expression(tokens):
 
 def math_or_string_expression(tokens):
     if tokens[0].isK(STRING):
-        return tokens[1:]
+        text = tokens[0].value()
+        return tokens[1:], text
     else:
-        return simple_expression(tokens)
+        after_expression_tokens = simple_expression(tokens)
+        i, text = 0, ""
+        while tokens[i] is not after_expression_tokens[0]:
+            text += str(tokens[i].value()) + " "
+            i += 1
+        return after_expression_tokens, text
 
 def attribute(tokens: List[Token]):
+    text = ""
     # init and assign
     check_token(tokens[0], ID)
+    text += tokens[0].value()
     if tokens[1].isK(EQUALS):
         check_token(tokens[2], TO)
-        tokens = math_or_string_expression(tokens[3:])
-        return tokens
+        tokens, expression_text = math_or_string_expression(tokens[3:])
+        text += ": " + expression_text
+        return tokens, text
     else:
-        return tokens[1:]
+        return tokens[1:], text
 
 def attribute_declaration(tokens: List[Token]):
     check_token(tokens[0], PRONOUN)
+    first = True
+    header_text = "def initialize("
+    text = ""
     if not tokens[1].isK(HAS):
-            return tokens
+        full_text = header_text + ")\n" + text + "end\n"
+        return tokens, full_text
     tokens = tokens[2:]
     while tokens[0].isK(ID):
-        tokens = attribute(tokens)
+        if first:
+            first = False
+        else:
+            header_text += ", "
+        text += "@" + tokens[0].value() + " = " + tokens[0].value() + "\n"
+        tokens, attribute_text = attribute(tokens)
+        header_text += attribute_text
         # end attribute declarations
         if tokens[0].isK(DOT):
-            return tokens[1:]
+            full_text = header_text + ")\n" + text + "end\n"
+            return tokens[1:], full_text
         check_token(tokens[0], COMMA)
         if tokens[1].value() == 'and':
             tokens = tokens[2:]
         else:
             tokens = tokens[1:]
-    return tokens
+    full_text = header_text + ")\n" + text + "end\n"
+    return tokens, full_text
 
 def method_declaration(tokens: List[Token]):
     check_token(tokens[0], PRONOUN)
     if not tokens[1].isK(CAN):
-            return tokens
+        return tokens
     tokens = tokens[2:]
     while tokens[0].isK(ID):
         # end method declarations
@@ -124,74 +146,104 @@ def method_declaration(tokens: List[Token]):
 def print_expression(tokens):
     check_token(tokens[0], PRONOUN)
     check_token(tokens[1], SAYS)
-    return math_or_string_expression(tokens[2:])
+    after_expression_tokens, expression_text = math_or_string_expression(tokens[2:])
+    text = "puts " + expression_text + "\n"
+    return after_expression_tokens, text
 
 def decrement_expression(tokens):
+    text = ""
     check_token(tokens[0], PRONOUN)
     check_token(tokens[1], DECREASES)
     if tokens[2].isK(POSESIVE):
         tokens = tokens[3:]
+        text += "@"
     else:
         tokens = tokens[2:]
     check_token(tokens[0], ID)
+    text += tokens[0].value()
     check_token(tokens[1], BY)
     check_token(tokens[2], NUM)
-    return tokens[3:]
+    text += " -= " + str(tokens[2].value()) + "\n"
+    return tokens[3:], text
 
 def increment_expression(tokens):
+    text = ""
     check_token(tokens[0], PRONOUN)
     check_token(tokens[1], INCREMENTS)
     if tokens[2].isK(POSESIVE):
         tokens = tokens[3:]
+        text += "@"
     else:
         tokens = tokens[2:]
     check_token(tokens[0], ID)
+    text += tokens[0].value()
     check_token(tokens[1], BY)
     check_token(tokens[2], NUM)
-    return tokens[3:]
+    text += " += " + str(tokens[2].value()) + "\n"
+    return tokens[3:], text
 
 def function_arguments(tokens):
+    text = ""
     check_token(tokens[0], ID)
+    text += tokens[0].value() + ": "
     check_token(tokens[1], AS)
-    tokens = math_or_string_expression(tokens[2:])
-
+    tokens, expression_text = math_or_string_expression(tokens[2:])
+    text += expression_text
     while tokens[0].isK(COMMA):
         if tokens[1].value() == 'and':
             if tokens[3].isK(AS):
                 check_token(tokens[2], ID)
-                tokens = math_or_string_expression(tokens[4:])
+                text += ", " + tokens[2].value()
+                tokens, expression_text = math_or_string_expression(tokens[4:])
+                text += ": " + expression_text
             else:
-                return tokens
+                return tokens, text
         else:
             if tokens[2].isK(AS):
                 check_token(tokens[1], ID)
-                tokens = math_or_string_expression(tokens[3:])
+                text += ", " + tokens[1].value()
+                tokens, expression_text = math_or_string_expression(tokens[3:])
+                text += ": " + expression_text
             else:
-                return tokens
-    return tokens
+                return tokens, text
+    return tokens, text
 
 def function_call_expression(tokens):
+    text = ""
     check_token(tokens[0], PRONOUN)
     check_token(tokens[1], ID)
+    text += tokens[1].value() + "("
     if tokens[2].isK(WITH):
-        return function_arguments(tokens[3:])
-    return tokens[2:]
+        after_arguments_tokens, arguments_text = function_arguments(tokens[3:])
+        text += arguments_text + ")\n"
+        return after_arguments_tokens, text
+    text += ")\n"
+    return tokens[2:], text
 
 def assignment_expression(tokens):
+    text = ""
     check_token(tokens[0],PRONOUN)
     check_token(tokens[1], LETS)
+    if tokens[2].isK(POSESIVE):
+        text += "@"
     tokens = tokens[3:] if tokens[2].isK(POSESIVE) else tokens[2:]
     check_token(tokens[0], ID)
+    text += tokens[0].value()
     check_token(tokens[1], BE)
     # IDEA: include function call? none return anything anyway
-    return math_or_string_expression(tokens[2:])
+    after_expression_tokens, expression_text = math_or_string_expression(tokens[2:])
+    text += " = " + expression_text + "\n"
+    return after_expression_tokens, text
+
 
 def inner_sentences(tokens):
-    tokens = sentence(tokens)
+    text = ""
+    tokens, sentence_text = sentence(tokens)
+    text += sentence_text
     if tokens[0].isK(DOT):
-        return tokens
+        return tokens, text
     if tokens[0].isK(SEMMI):
-        return tokens
+        return tokens, text
     check_token(tokens[0], COMMA)
     if tokens[1].value() == 'and':
         return inner_sentences(tokens[2:])
@@ -201,14 +253,20 @@ def inner_sentences(tokens):
 def conditional_expression(tokens):
     check_token(tokens[0], IF)
     # TODO: Add booleans (TRUE, FALSE) and boolean operators (and, or, not)
-    tokens = math_or_string_expression(tokens[1:])
-    return inner_sentences(tokens)
+    tokens, expression_text = math_or_string_expression(tokens[1:])
+    text = "if " + expression_text + "\n"
+    tokens, inner_text = inner_sentences(tokens)
+    return tokens, text + inner_text
 
 def loop_expression(tokens):
     check_token(tokens[0], WHILE)
+    text = "while "
     # TODO: Add booleans (TRUE, FALSE) and boolean operators (and, or, not)
-    tokens = math_or_string_expression(tokens[1:])
-    return inner_sentences(tokens)
+    tokens, expression_text = math_or_string_expression(tokens[1:])
+    text += expression_text + "\n"
+    after_inner_tokens, inner_text = inner_sentences(tokens)
+    text += inner_text + "end\n"
+    return  after_inner_tokens, text
 
 def sentence(tokens: List[Token]):
     if tokens[1].isK(INCREMENTS):
@@ -227,52 +285,75 @@ def sentence(tokens: List[Token]):
         return loop_expression(tokens)
 
 def sentences(tokens: List[Token]):
-    tokens = sentence(tokens)
+    tokens, sentence_text = sentence(tokens)
     if tokens[0].isK(DOT):
-        return tokens[1:]
+        return tokens[1:], sentence_text
     check_token(tokens[0], SEMMI)
     if tokens[1].value() == 'and':
-        return sentences(tokens[2:])
+        after_sentences_tokens, sentences_text = sentences(tokens[2:])
+        return after_sentences_tokens, sentence_text + sentences_text
     else:
-        return sentences(tokens[1:])
+        after_sentences_tokens, sentences_text = sentences(tokens[1:])
+        return after_sentences_tokens, sentence_text + sentences_text
 
 def params(tokens: List[Token]):
+    text = ""
     check_token(tokens[0], ID)
+    first = True
     while tokens[0].isK(ID):
         # end params
+        if not first:
+            text += ", "
+        else:
+            first = False
+        text += str(tokens[0].value()) + ":"
         if tokens[1].isK(SEMMI):
-            return tokens[2:]
+            return tokens[2:], text
         check_token(tokens[1], COMMA)
         if tokens[2].value() == 'and':
             tokens = tokens[3:]
         else:
             tokens = tokens[2:]
-    return tokens
+    return tokens, text
 
 def method_description(tokens: List[Token]):
-    while tokens and tokens[0].isK(TO):
+    text = ""
+    while len(tokens) > 0 and tokens[0].isK(TO):
         check_token(tokens[0], TO)
         check_token(tokens[1], ID)
+        text += "def " + str(tokens[1].value()) + "("
         if tokens[2].isK(PRONOUN) and tokens[3].isK(NEEDS):
-            tokens = params(tokens[4:])
-            tokens = sentences(tokens)
+            tokens, params_text = params(tokens[4:])
+            text += params_text + ")\n"
+            tokens, sentences_text = sentences(tokens)
+            text += sentences_text
+            text += "end\n"
         else:
-            tokens = sentences(tokens[2:])
-    return tokens
+            text += ")\n"
+            tokens, sentences_text = sentences(tokens[2:])
+            text += sentences_text
+            text += "end\n"
+    return tokens, text
 
 def class_def(tokens: List[Token]):
-    tokens = class_declaration(tokens)
+    tokens, class_declaration_text = class_declaration(tokens)
     check_token(tokens[0], DOT)
-    tokens = attribute_declaration(tokens[1:])
+    tokens, attribute_declaration_text = attribute_declaration(tokens[1:])
     tokens = method_declaration(tokens)
-    tokens = method_description(tokens)
-    return tokens
+    tokens, method_description_text = method_description(tokens)
+    text = class_declaration_text + attribute_declaration_text + method_description_text
+    return tokens, text
 
 def classes(tokens: List[Token]):
-    tokens = class_def(tokens)
+    text = ""
+    tokens, class_text = class_def(tokens)
+    text += class_text
     while tokens and tokens[0].isK(LINE_BREAK):
-        tokens = class_def(tokens)
-    print(tokens)
+        class_text = ""
+        tokens, class_text = class_def(tokens)
+        text += class_text
+    print("Text =\n", text)
+    return text
 
 class Parser(object):
     def __init__(self, tokens: list, to_ruby=False):
